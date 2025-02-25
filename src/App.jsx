19d8f89da2b257868a251450c5c6a473d25cbad5
zoriah <noa351@hotmail.de>
2025-02-25 11:20:46 +0100

    add feature/bottomnav

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "./Layouts/MainLayout";
 import Header from "./components/Header";
+import Bottomnav from "./components/Bottomnav";
+import Login from "./components/Login";
 
 const App = () => {
   return (
     <>
-      <Header />
-      {/* <Routes>
-        <Route path='/' element={<MainLayout />}>
-          <Route index element={<h2>HOME</h2>} />
+      {/* <Header /> */}
 
-          <Route path='*' element={<h2>Not Found</h2>} />
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Bottomnav />} />
+          <Route path="login" element={<Login />} />
+
+          <Route path="*" element={<h2>Not Found</h2>} />
         </Route>
-      </Routes> */}
+      </Routes>
     </>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <input type="checkbox" defaultChecked className="checkbox" />
+      {/* <input type="checkbox" defaultChecked className="checkbox" /> */}
     </div>
   );
 };
