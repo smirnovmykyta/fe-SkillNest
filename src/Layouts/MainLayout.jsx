79376a19663d41738e7f861419b7f719bb1f00ff
ilonavich <ilonavich@yahoo.de>
2025-02-25 13:53:06 +0100

    Anzeigenkarten

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Cards from "../components/Cards";
 
 const MainLayout = () => {
   return (
     <>
       <Header />
-
+      <Cards />
       <Outlet />
     </>
   );
