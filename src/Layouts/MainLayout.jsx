030875acc68ba702c91abc74c46bbf9cc9550903
Smirnov <smirnov.mykyta@gmail.com>
2025-02-28 14:03:55 +0100

    fix styling by outlet

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,18 +1,20 @@
-import { Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
 import Header from "../components/Header";
 import Searchbar from "../components/Searchbar";
 import Bottomnav from "../components/Bottomnav";
 
 const MainLayout = () => {
-  return (
-    <>
-      <Header />
-      <Searchbar />
-      <Outlet />
-      <Bottomnav />
-      {/* <Footer /> */}
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <Searchbar/>
+            <div className=" pb-16">
+                <Outlet/>
+            </div>
+            <Bottomnav/>
+            {/* <Footer /> */}
+        </>
+    );
 };
 
 export default MainLayout;
