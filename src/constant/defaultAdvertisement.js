69b3cc9a2cbccd601f8498356a2ace59bc5ce3d9
Smirnov <smirnov.mykyta@gmail.com>
2025-02-28 13:52:52 +0100

    refactoring

--- src/constant/defaultAdvertisement.js
+++ src/constant/defaultAdvertisement.js
@@ -2,6 +2,7 @@ import {category} from "./category.js";
 import {lessonMode} from "./lessonMode.js";
 
 export const defaultAdvertisement = {
+  userId: "67c1822f5d50abbd34fc30ed",
   media: [],
   title: "",
   description: "",
