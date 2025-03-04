902b0271171c458d872063df46e1d810a5cc2f66
ilonavich <ilonavich@yahoo.de>
2025-03-04 22:35:50 +0100

    Searchbar: adjustment, design, responsivenes implemented

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,32 +2,26 @@ import React from "react";
 
 const Searchbar = () => {
   return (
-    <div className="searchbar p-4">
-      <div className="form-control">
-        <div className="input-group flex justify-center">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="input input-bordered max-w-3xl"
-          />
-          <button className="btn btn-square">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
+    <div className="j">
+      <label className="input ustify-center max-w-4xl lg:w-3xl mx-auto mb-5 flex items-center gap-2 p-2 border border-gray-200 rounded-full ">
+        <svg
+          className="h-[1em] opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </g>
+        </svg>
+        <input type="search" className="grow" placeholder="Search" />
+      </label>
     </div>
   );
 };
