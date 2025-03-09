4d720b1bf9da168bf0163a8be31b0482356d8fcd
ilonavich <ilonavich@yahoo.de>
2025-03-09 23:27:03 +0100

    EditProfile.jsx: changed color of button to indigo500

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -148,7 +148,7 @@ const EditProfile = ({ user, setUser }) => {
         {/* Edit and Save Buttons */}
         <div className="flex justify-end space-x-2 mt-4">
           <button
-            className="px-4 py-2 bg-green-500 text-white rounded"
+            className="px-4 py-2 bg-indigo-500 text-white rounded"
             type="submit"
           >
             Save Changes
