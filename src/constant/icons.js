2464e56ed43c658b672b9ca0eec3cc64a9e89cc5
ilonavich <ilonavich@yahoo.de>
2025-03-11 14:57:40 +0100

    Navbar: Chat gelöscht

--- src/constant/icons.js
+++ src/constant/icons.js
@@ -8,7 +8,6 @@ const icons = [
   { name: "Search", icon: BiSearchAlt, link: "/" },
   { name: "Favorites", icon: GrFavorite, link: "/favorites" },
   { name: "Ad", icon: AiOutlineFileAdd, link: "/ad" },
-  { name: "Chat", icon: LuMessageCircleMore, link: "/chat" },
   { name: "Profile", icon: LuCircleUser, link: "/profile" },
 ];
 
