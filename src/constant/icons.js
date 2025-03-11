2b81b2cf1771464cc682d0c6ccbd4f61f1b49018
Smirnov <smirnov.mykyta@gmail.com>
2025-03-11 15:36:27 +0100

    cut out chat from nav bar

--- src/constant/icons.js
+++ src/constant/icons.js
@@ -8,7 +8,7 @@ const icons = [
   { name: "Search", icon: BiSearchAlt, link: "/" },
   { name: "Favorites", icon: GrFavorite, link: "/favorites" },
   { name: "Ad", icon: AiOutlineFileAdd, link: "/ad" },
-  { name: "Chat", icon: LuMessageCircleMore, link: "/chat" },
+  /*{ name: "Chat", icon: LuMessageCircleMore, link: "/chat" },*/
   { name: "Profile", icon: LuCircleUser, link: "/profile" },
 ];
 
