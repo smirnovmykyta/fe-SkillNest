18f47c127d047406ccf87fd043eb69c3df00a6df
Smirnov <smirnov.mykyta@gmail.com>
2025-03-11 18:41:49 +0100

    deleted unused import

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getUserById } from "../api/userApi.js";
 import FavoriteToggle from "./FavoriteToggle";
 
 const Card = ({ card }) => {
